b43429962a909d0450efdd5f449b8ef8f166f96b
Steven Kim <stevenkim1989@gmail.com>
2022-04-30 02:32:14 -0700

    edit datamodel and add many to many relationship

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -16,8 +16,31 @@ namespace Avesdo.Data
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
+            
+            builder.Entity<OrdPiz>()
+                .HasOne(o => o.Order)
+                .WithMany(op => op.OrdPizs)
+                .HasForeignKey(f => f.Order_id);
+            builder.Entity<OrdPiz>()
+                .HasOne(p => p.Pizza)
+                .WithMany(op => op.OrdPizs)
+                .HasForeignKey(f => f.Pizza_id);
+
+            builder.Entity<PizTop>()
+                .HasOne(p => p.Pizza)
+                .WithMany(pt => pt.PizTops)
+                .HasForeignKey(f => f.Pizza_title);
+            builder.Entity<PizTop>()
+                .HasOne(t => t.Topping)
+                .WithMany(pt => pt.PizTops)
+                .HasForeignKey(f => f.Topping_title);
         }
 
         public DbSet<Pizzas> Pizzas { get; set; }
+        public DbSet<Customers> Customers { get; set; }
+        public DbSet<Orders> Orders { get; set; }
+        public DbSet<Toppings> Toppings { get; set; }
+        public DbSet<OrdPiz> OrdPizs { get; set; }
+        public DbSet<PizTop> PizTops { get; set; }
     }
 }
