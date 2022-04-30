df2fb6308211e5daff16b11961d4d66ee32fb878
Steven Kim <stevenkim1989@gmail.com>
2022-04-29 23:38:11 -0700

    .net core 5.0 template with gitignore

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Avesdo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
